5301db20fb0e30d715de03ee16701e7e429145b5
Gyeongjun Jang <gyeongjun96@gmail.com>
2025-03-20 06:41:05 +0900

    3rd commit
    부하 테스트 locustfile, 주석 추가

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpUser, task
 class ServerTest(HttpUser):
     @task
     def api_test(self):
-        self.client.get("/api/status")
-        self.client.get("/api/resource")
-        self.client.get("/api/data")
-        self.client.get("/actuator/health")
+        self.client.get("/api/status") # 상태 응답 테스트
+        self.client.get("/api/resource") # 리소스 집약적 API 테스트
+        self.client.get("/api/data") # 데이터 처리 API 테스트
+        self.client.get("/actuator/health") # 서버 상태 확인
